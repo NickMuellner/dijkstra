b90e73e59470b3bfc50535a70631f5d6f10307a2
Marco Auenhammer <marco@auenhammer.at>
2024-06-06 12:44:25 +0200

    Adjusted output

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
             graph.readFile(args[0]);
             //graph.nodes.forEach((e, f) -> System.out.println(f));
             System.out.println(graph.dijkstra(args[1], args[2]).stream().map(e -> e.name).collect(Collectors.joining(" - ")));
-            System.out.println(graph.getEndNodeDistance());
+            System.out.println("Journey length in minutes: " + graph.getEndNodeDistance());
         }else{
             System.err.println("Invalid usage!");
         }
